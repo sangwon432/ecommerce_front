75e41cdd33d6b93b3b7461a7bad1bfc209a119bb
sangwon432 <swonl0622@gmail.com>
2024-03-21 09:24:26 -0500

    프로필 페이지 수정

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import axios, {create} from "axios";
-import {Card, CardBody, Col, Container, Row} from "react-bootstrap";
+import {Button, Card, CardBody, Col, Container, Form, Row} from "react-bootstrap";
 
 const Profile = () => {
 
@@ -8,6 +8,9 @@ const Profile = () => {
     const [name, setName] = useState("")
     const [profileImg, setProfileImg] = useState("")
     const [createdAt, setCreatedAt] = useState("")
+
+    const [file, setFile] = useState()
+
 
     const getProfile = async () => {
         try {
@@ -43,50 +46,36 @@ const Profile = () => {
     }, []);
 
     return (
-        <Container fluid>
+        <Container className={"mt-5"}>
+            <h1>Profile</h1>
             <Row>
-                <Col xl={"4"}>
-                    <Card className={"overflow-hidden"}>
-                        <div className={"bg-primary bg-soft"}>
-                            <Row>
-                                <Col xl={"7"}>
-                                    <div className={"text-primary p-3"}>
-                                        <h5 className={"text-primary"}>Welcome Back!!!</h5>
-                                        <p>블라블라</p>
-                                    </div>
-                                </Col>
-                                {/*<Col xs={"5"} className={"align-self-end"}>*/}
-                                {/*    <img src={profileImg} alt={email} className={"img-fluid"}/>*/}
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
+                            disabled
+                        />
+                    </Form.Group>
 
-                                {/*</Col>*/}
-                            </Row>
-                        </div>
-                        <CardBody className={"pt-0"}>
-                            <Row>
-                                <Col sm={"4"}>
-                                    <div className={"avatar-md profile-user-wid mb-4"}>
-                                        <img
-                                            src={profileImg}
-                                            alt={email}
-                                            className={"img-thumbnail rounded-circle"}
-                                        />
-                                    </div>
-                                    <h5 className={"font-size-15 text-truncate"}>
-                                        {name}
-                                    </h5>
-                                    <p className={"text-muted mb-0 text-truncate"}>
-                                        {email}
-                                    </p>
-                                    <p className={"text-muted mb-0 text-truncate"}>
-                                        가입일: {createdAt.slice(0, 10)}
-                                    </p>
-                                </Col>
-                            </Row>
-                        </CardBody>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter email"
+                            value={name}
+                            onChange={(e)=> setName(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Button>
+                        프로필 정보 수정
+                    </Button>
+                </Form>
 
-                    </Card>
 
-                </Col>
             </Row>
         </Container>
     );
